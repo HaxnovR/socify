df083d3801af7779795a55a50f90b58100db78c5
haxnovr <mayankgajwe@gmail.com>
2022-01-26 23:25:13 +0530

    init

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
+const SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi({
+    clientId: process.env.spotify_id,
+    clientSecret: process.env.spotify_secret,
+});
+
+console.log("Test");
