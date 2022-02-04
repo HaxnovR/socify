0bd6181f9e2d5ee3603ff9c3a337a8f6299b9118
haxnovr <mayankgajwe@gmail.com>
2022-02-04 20:04:02 +0530

    init

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use('/auth/**', 
         createProxyMiddleware({ 
-            target: 'http://localhost:5000'
-            // target: 'https://testsocify.herokuapp.com:80'
+            // target: 'http://localhost:5000'
+            target: 'https://testsocify.herokuapp.com'
         })
     );
 };
