b4e3e28b4d0493311f6501349385f85687f3cd2d
HaxnovR <mayankgajwe@gmail.com>
2022-06-20 03:34:06 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,125 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+var access_token = '';
+var refresh_token = '';
+
+dotenv.config();
+
+const port = 5000;
+console.log('Found on PORT =',port);
+
+const app = express();
+
+const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const spotify_redirect_uri = 'https://testsocify.herokuapp.com/'
+// const spotify_redirect_uri = 'http://localhost:3000/'
+
+app.use(cors());
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Run Static builded website
+// Update Build in root folder
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+const randomState = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+const scopes = ['user-read-private',
+  'user-read-email',
+  'user-modify-playback-state',
+  'user-read-playback-state',
+  'user-read-currently-playing',
+  'streaming',
+  'user-read-recently-played'],
+  redirectUri = spotify_redirect_uri,
+  clientId = spotify_client_id,
+  state = randomState(16),
+  showDialog = true;
+
+// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+var spotifyApi = new SpotifyWebApi({
+  redirectUri: redirectUri,
+  clientId: clientId,
+  clientSecret: spotify_client_secret
+});
+
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state, showDialog);
+
+console.log(authorizeURL);
+
+app.get('/auth/login', (req, res) => {
+    var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state, showDialog);
+    res.redirect(authorizeURL.toString());
+})
+
+app.post('/auth/cred', (req,res) => {
+  //  setup 
+  
+  //  Get the "code" value posted from the client-side and get the user's accessToken from the spotify api     
+      const code = req.body.code
+  
+      // Retrieve an access token
+      spotifyApi.authorizationCodeGrant(code).then((data) => {
+  
+          // Returning the User's AccessToken in the json formate  
+          res.json({
+              accessToken : data.body.access_token,
+          }) 
+      })
+      .catch((err) => {
+          console.log(err);
+          res.sendStatus(400)
+      })
+  
+  })
+
+// app.get('/auth/callback', (req, res) => {
+
+//     var code = req.query.code;
+//     res.send(code);
+    
+//     spotifyApi.authorizationCodeGrant(code).then(
+//     function(data) {
+  
+//       // Set the access token on the API object to use it in later calls
+//       spotifyApi.setAccessToken(data.body['access_token']);
+//       spotifyApi.setRefreshToken(data.body['refresh_token']);
+//       access_token = data.body.access_token;
+//       refresh_token = data.body.refresh_token;
+//       res.redirect('/');
+//     },
+//     function(err) {
+//       console.log('Something went wrong!', err);
+//     }
+//   );
+// });
+
+// app.get('/auth/token', (req, res) => {
+//   res.json({ access_token: access_token,
+//     refresh_token: refresh_token
+//   });
+// });
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+});
